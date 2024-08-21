77f30b3f4b2b9e64ab7c19d7146249f485777c81
Ayesh Kadike <146994029+ayeshkadike@users.noreply.github.com>
2024-08-21 14:28:43 +0400

    Update weather_page.dart
    Enter API KEY

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -11,7 +11,7 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-  final _weatherService = WeatherService('271fdce6c47409e4ed140488b27618c2');
+  final _weatherService = WeatherService('YOUR_API_KEY');
   Weather? _weather;
 
   _fetchWeather() async {
